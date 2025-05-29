e321fc62c19270a8330cc521e6c7a2cd556d8bf0
haiphan <hai.phan@zoho.com>
2025-05-29 19:53:37 +0300

    Add gh workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const DEFAULT_INITIAL_COUNT = 0;
+const initialCount = process.env.REACT_APP_INITIAL_COUNT
+    ? parseInt(process.env.REACT_APP_INITIAL_COUNT, 10)
+    : DEFAULT_INITIAL_COUNT;
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(initialCount);
 
   const increment = () => {
     setCount(count + 1);
