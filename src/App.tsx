00758c995d6d91225e2b964b7293124b7b7ac892
haiphan <hai.phan@zoho.com>
2025-05-29 19:33:43 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Counter: {count}</p>
+        <button onClick={increment}>Increment</button>
       </header>
     </div>
   );
