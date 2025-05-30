8c1376af7dcafc06c41146f619c8c699648bb112
haiphan <hai.phan@zoho.com>
2025-05-30 11:48:31 +0300

    Update script

--- scripts/update_env.sh
+++ scripts/update_env.sh
@@ -3,8 +3,8 @@
 # Get the current value of REACT_APP_INITIAL_COUNT from the environment
 current_count="${REACT_APP_INITIAL_COUNT}"
 
-# Toggle the count: 0 to 1, 1 to 0
-new_count=$((1 - current_count))
+# Update the count, cycling through 0-9
+new_count=$(( (1 + current_count) % 10 ))
 
 # Set the new environment variable for subsequent steps
 echo "REACT_APP_INITIAL_COUNT=$new_count" >> "$GITHUB_ENV"
